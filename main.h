3e0949f76de943df8530d386000f65b486f019b7
aefayoub <aef.ayoub@gmail.com>
2023-07-13 11:32:01 -0700

    cmt main.h

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+
+
+#endif
