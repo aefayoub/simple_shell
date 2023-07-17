4cc6ede5ceab15a59ce72c235ef2b303c6868069
aefayoub <aef.ayoub@gmail.com>
2023-07-17 07:13:56 -0700

    cmt new changes

--- main.h
+++ main.h
@@ -14,19 +14,92 @@
 
 #define BUFFER_SIZE 1024
 
+/**
+ * struct data - struct that contains all relevant data on runtime
+ * @av: argument vector
+ * @input: command line written by the user
+ * @args: tokens of the command line
+ * @status: last status of the shell
+ * @counter: lines counter
+ * @_environ: environment variable
+ * @pid: process ID of the shell
+ */
+typedef struct data
+{
+	char **av;
+	char *input;
+	char **args;
+	int status;
+	int counter;
+	char **_environ;
+	char *pid;
+} data_shell;
+
+/**
+ * struct sep_list_s - single linked list
+ * @separator: ; | &
+ * @next: next node
+ * Description: single linked list to store separators
+ */
+typedef struct sep_list_s
+{
+	char separator;
+	struct sep_list_s *next;
+} sep_list;
+
+/**
+ * struct line_list_s - single linked list
+ * @line: command line
+ * @next: next node
+ * Description: single linked list to store command lines
+ */
+typedef struct line_list_s
+{
+	char *line;
+	struct line_list_s *next;
+} line_list;
+
+/**
+ * struct r_var_list - single linked list
+ * @len_var: length of the variable
+ * @val: value of the variable
+ * @len_val: length of the value
+ * @next: next node
+ * Description: single linked list to store variables
+ */
+typedef struct r_var_list
+{
+	int len_var;
+	char *val;
+	int len_val;
+	struct r_var_list *next;
+} r_var;
+
+/**
+ * struct builtin_s - Builtin struct for command args.
+ * @name: The name of the command builtin i.e cd, exit, env
+ * @f: data type pointer function.
+ */
+typedef struct builtin_s
+{
+	char *name;
+	int (*f)(data_shell *datash);
+} builtin_t;
+
+/* 0-str.c */
 char *_strConcatenate(char *dest, const char *src);
 char *_strCopy(char *dest, char *src);
 int _strCompares(char *s1, char *s2);
 char *_strCharacterLocator(char *s, char c);
 int _strLength(char *s, char *accept);
 
+/* 1-str.c */
 char *_strDuplicates(const char *s);
 int _strLenghtofString(const char *s);
 int compareChars(char str[], const char *delim);
 char *_strSpliter(char str[], const char *delim);
 int _strIfDigital(const char *s);
 
-void strReversing(char *s);
 
 
 #endif /* MAIN_H */
