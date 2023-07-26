463e7c2cd53148500c2a05156b3530f01a23247d
aefayoub <aef.ayoub@gmail.com>
2023-07-26 05:59:38 -0700

    cmt changes

---
+++ 0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+        printf("print string"); 
+        return 0;
+}
